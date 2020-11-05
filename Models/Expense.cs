4576d62ffca1dc44a3f4dae575f2a818cb6f1471
Tobias Victor <bboyvictor17@yahoo.com>
2020-11-05 20:21:33 +0200

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace To_Vi_Lab5
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+
+        public string Secret { get; set; }
+
+    }
+}
---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace To_Vi_Lab5.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
